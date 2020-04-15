453334cc5cf3f6107704a212f9660ee4198d84c0
Guillermo Cabrera <guicaro@gmail.com>
2020-04-14 23:12:17 -0500

    Fixed contact form, linked with formspree

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
       submitHandler: function(form) {
         $.ajax({
           type: 'POST',
-          url: 'send.php',
+          url: 'https://formspree.io/xjvelpyb',
           data: $(form).serialize(),
           success: function(data) {
             if(data.match(/success/)) {
